3ddf48ebfbc686035ffeff1c60914f2285f15ead
David Stepanic <davidstepanic9@gmail.com>
2025-12-03 15:50:43 +0100

    Cleand repo accoridng to .gitignore

--- src/functions.clj
+++ src/functions.clj
@@ -1,7 +1,7 @@
 (ns functions)
 
-(defn -main []
-   (println (apply / (reduce (fn [[sum cnt] e]
-                               [(+ sum e) (inc cnt)])
-                             [0 0]
-                             [24 23 30]))))
+
+ (apply / (reduce (fn [[sum cnt] e]
+                       [(+ sum e) (inc cnt)])
+                           [0 0]
+                           [10 20 30]))
