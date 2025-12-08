61def7ba6d7f35295e454efdfe949861076d6828
David Stepanic <davidstepanic9@gmail.com>
2025-12-08 16:37:57 +0100

    Advent of Code Day 1 - part two finished

--- src/homework.clj
+++ src/homework.clj
@@ -22,27 +22,41 @@
 
 ; Advent of Code Day 1 - part two
 
+; CLASS VERSION - didn't check if we start from zero, and also algorithm
+; was increasing counter even if safe stops on zero, which is wrong because
+; result includes only if we during rotations pass zero
 
-(reduce (fn [[cnt zed sum] e]
+(reduce (fn [[cnt move sum] e]
           [(if (= (mod (+ sum e) 100) 0)
              (inc cnt)
              cnt)
            (if (< (+ sum e) 0)
-             (inc zed)
-             zed)
+             (+ (abs  (- 1 (quot (+ sum e) 100))) move)
+             (+ (quot (+ sum e) 100) move))
            (mod (+ sum e) 100)])
         [0 0 50]
-        [-68 -30 48 -5 60 -55 -1 -99 14 -82])
+        [-370])
 
-(reduce + (take 2 *1))
+; Solution
 
+(reduce (fn [[cnt move sum] e]
 
-
-
-
-
-
-
-
-
+          (println sum)
+          [(if (= (mod (+ sum e) 100) 0)
+             (inc cnt)
+             cnt)
+           (if (<= (+ sum e) 0)
+             (+ (if (= (mod (+ sum e) 100) 0)
+                  (abs (quot (abs(+ sum e)) 100))
+                  (abs (- 1 (quot (+ sum e) 100))))
+                (if (= sum 0)
+                  (- move 1)
+                  move))
+             (+ (quot (+ sum e) 100)
+                (if (= (mod (abs(+ sum e)) 100) 0)
+                  (- move 1)
+                  move)))
+           (mod (+ sum e) 100)])
+        [0 0 50]
+        [-50])
 
